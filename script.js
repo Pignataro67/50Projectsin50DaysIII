b093ce8092a8c8ecc602ec92851806a4759b198b
Pignataro67 <Pignataro67@gmail.com>
2021-01-06 07:44:48 -0800

    JS for project #17, Movie App

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5e6bff04ee0ce08c9544b7433ef0038a&page=1'
+// const IMG_PATH = 'https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=5e6bff04ee0ce08c9544b7433ef0038a&query="'
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
+//Get initial Movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  showMovies(data.results)
+}
+
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+    <h3>${title}</h3>
+    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+    <h3>Overview</h3>
+    ${overview}
+    </div>
+    `
+    main.appendChild(movieEl)
+  })
+}
+
+function getClassByRate(vote) {
+  if(vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+
+  if(searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
 
 // const smallCups = document.querySelectorAll('.cup-small')
 // const liters = document.getElementById('liters')
