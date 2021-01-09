a3b671ca1307306fe34214f355e4b767b0105c5c
Pignataro67 <Pignataro67@gmail.com>
2021-01-09 14:11:14 -0800

    adding a function, improving readability/spacing

--- script.js
+++ script.js
@@ -2,23 +2,38 @@ const body = document.body
 const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
+
 let activeSlide = 0
 
 rightBtn.addEventListener('click', () => { 
   activeSlide++
+
     if (activeSlide > slides.length - 1) { 
-    activeSlide = 0 }
+      activeSlide = 0 
+    }
  
     setBgToBody() 
-    setActiveSlide()})
+    setActiveSlide()
+  })
 
-    leftBtn.addEventListener('click', () => { 
-      activeSlide--
-        if (activeSlide < 0) { 
-          activeSlide = slides.length - 1 }
+  leftBtn.addEventListener('click', () => { 
+    activeSlide--
+    
+    if (activeSlide < 0) { 
+      activeSlide = slides.length - 1 
+    }
     
     setBgToBody() 
-    setActiveSlide()})
+    setActiveSlide()
+  })
+
+  setBgToBody()
+
+  function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+  }  
+
+
 
 // const body = document.body
 // const slides = document.querySelectorAll('.slide')
