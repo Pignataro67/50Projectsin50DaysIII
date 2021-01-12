4bf5fa909d294ed0afe5d068656b789ee054250f
Pignataro67 <Pignataro67@gmail.com>
2021-01-12 11:19:19 -0800

    debug- light mode working

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ toggle.addEventListener('click', (e) => {
   const html = document.querySelector('html')
   if (html.classList.contains('dark')) {
     html.classList.remove('dark')
-    e.target.innerHTML = 'Dark Mode'
+    e.target.innerHTML = 'Dark mode'
   } else {
     html.classList.add('dark')
-    e.target.innerHTML = 'Light Mode'
+    e.target.innerHTML = 'Light mode'
   }
 })
 
