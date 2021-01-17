7f11cab6ed88ce23eccfa7b88b3e8f5838f9b64a
Pignataro67 <Pignataro67@gmail.com>
2021-01-17 08:17:45 -0800

    adding a function

--- script.js
+++ script.js
@@ -56,6 +56,14 @@ increaseBtn.addEventListener('click', () => {
  updateSizeOnScreen()
 })
 
+decreaseBtn.addEventListener('click', () => { 
+  size -= 5
+  
+  if(size < 5) { 
+    size = 5 
+  }
+})
+
 // const smallCups = document.querySelectorAll('.cup-small')
 // const liters = document.getElementById('liters')
 // const percentage = document.getElementById('percentage')
