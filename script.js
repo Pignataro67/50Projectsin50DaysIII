487ba067fc1807049932907094f09c448e415f1c
Pignataro67 <Pignataro67@gmail.com>
2021-01-20 14:21:13 -0800

    adding JS for Project #25, Sticky Navbar

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
 
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add('active')
+  } else {
+    nav.classList.remove('active')
+  }
+}
 
 // colorEl.addEventListener('change', (e) => color = e.target.value)
 
